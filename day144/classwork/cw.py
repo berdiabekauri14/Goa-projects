c288acfefc89d0702628a9808e54be6f638b295e
Berdia Bekauri <berdiabekauri@Berdias-MacBook-Air.local>
2025-04-15 12:35:00 +0400

    commit

---
+++ day144/classwork/cw.py
@@ -0,0 +1,75 @@
+# Codewars-ები გვქონდა გასაკეთებლად
+
+# 1)
+
+def say_hello(name):
+    return f"Hello, {name}"
+
+# 2)
+
+# function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+#   return laLigaGoals + copaDelReyGoals + championsLeagueGoals
+# }
+
+# 3)
+
+# function howMuchILoveYou(nbPetals) {
+#   if (nbPetals === 1) {
+#     return "I love you";
+#   } else if (nbPetals === 2) {
+#     return "a little";
+#   } else if (nbPetals === 3) {
+#     return "a lot";
+#   } else if (nbPetals === 4) {
+#     return "passionately";
+#   } else if (nbPetals === 5) {
+#     return "madly";
+#   } else {
+#     return "not at all"
+#   }
+# }
+
+# 4)
+
+def greet(language):
+    if language == 'english':
+        return "Welcome"
+    elif language == 'dutch':
+        return "Welkom"
+    elif language == 'IP_ADRESS_INVALID':
+        return "Welcome"
+    elif language == '':
+        return 'Welcome'
+    elif language == 'estonian':
+        return 'Tere tulemast'
+    elif language == 'danish':
+        return 'Velkomst'
+    elif language =='lithuanian':
+        return "Laukiamas"
+    elif language == 'welsh':
+        return 'Croeso'
+    elif language == 'french':
+        return 'Bienvenue'
+    elif language == 'german':
+        return 'Willkommen'
+    elif language == 'irish':
+        return 'Failte'
+    elif language == 'swedish':
+        return 'Valkommen'
+    elif language == 'spanish':
+        return 'Bienvenido'
+    elif language == 'italian':
+        return 'Benvenuto'
+    elif language == 'flemish':
+        return 'Welgekomen'
+    elif language == 'polish':
+        return 'Witamy'
+    elif language == 'finnish':
+        return 'Tervetuloa'
+    elif language == 'czech':
+        return 'Vitejte'
+    elif language == 'latvian':
+        return 'Gaidits'
+    
+    else:
+        return 'Welcome'
