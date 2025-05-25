575eaa455c42e59e5e761bdc1a2a427fd2df9100
Berdia Bekauri <berdiabekauri@Berdias-MacBook-Air.local>
2025-05-25 23:23:54 +0400

    commit

---
+++ day129/homework/src/components/Profile.jsx
@@ -0,0 +1,8 @@
+export default function Profile() {
+    return (
+        <main>
+            <h1>About me</h1>
+            <p>my name is berdia bekauri, i am 12 years old, i am at VI grade and turning to VII grade. my hobbies are programming, playing chess and football, looking at the planets with my telescope, doing maths and reading books.</p>
+        </main>
+    )
+}
